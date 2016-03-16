1eaf11e249965f5f3d6fe918efd1aa55141e9f06
David Oxley <david.oxley@intelematics.com>
2016-03-16 11:43:12 +1100

    Add QUEUE to startup script.

--- startup.sh
+++ startup.sh
@@ -3,6 +3,7 @@
 sed -i s/__buildkite_token__/$BUILDKITE_TOKEN/g /conf/hooks.json
 
 cat <<EOF >/etc/cron.d/buildkite-metrics-publisher
+QUEUE=$QUEUE
 BUILDKITE_API_ACCESS_TOKEN=$BUILDKITE_API_ACCESS_TOKEN
 BUILDKITE_ORG_SLUG=$BUILDKITE_ORG_SLUG
 AWS_REGION=$AWS_REGION
